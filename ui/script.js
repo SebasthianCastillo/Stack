4b08800bb843a90d0ebe0c7665642e761ea72d6e
Seb <scastillo.hgo@gmail.com>
2024-08-02 00:20:04 -0400

    script js

--- ui/script.js
+++ ui/script.js
@@ -1,30 +1,34 @@
 // import { fetch } from "undici-types";
 
 let ctx = document.getElementById('myChart');
+let mychart;
+let jsondata;
 
 fetch('http://127.0.0.1:3000/dbz')
 
   .then(function (response) {
     if (response.ok) {
-
       return response.json();
-
     }
   })
   .then(function (data) {
-    console.log(data)
+    jsondata = data;
     createChart(data, 'bar');
   })
 
-
+function setChartType(type) {
+  mychart.destroy();
+  createChart(jsondata, type)
+}
 function createChart(data, type) {
-  new Chart(ctx, {
+
+  mychart = new Chart(ctx, {
     type: type,
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: data.map(c => c.name),
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Ki',
+        data: data.map(c => c.ki.replaceAll('.', '')),
         borderWidth: 1
       }]
     },
@@ -32,6 +36,7 @@ function createChart(data, type) {
       scales: {
         y: {
           beginAtZero: true
+
         }
       }
     }
